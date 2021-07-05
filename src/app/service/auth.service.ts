c0ea7fbb22648dfa63a8afa37967136d969f2b69
Elton Santos <elton.santos07@outlook.com>
2021-07-05 01:19:33 -0300

    Componente tema sem consumo

--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -16,15 +16,15 @@ export class AuthService {
     ) { }
 
   entrar(userLogin: UserLogin): Observable<UserLogin>{
-    return this.http.post<UserLogin>('https://backendthiagofaccipieri.herokuapp.com/usuarios/logar', userLogin)
+    //return this.http.post<UserLogin>('https://backendthiagofaccipieri.herokuapp.com/usuarios/logar', userLogin)
     
-    //return this.http.post<UserLogin>('https://meublogpessoal.herokuapp.com/usuarios/logar', userLogin)
+    return this.http.post<UserLogin>('https://meublogpessoal.herokuapp.com/usuarios/logar', userLogin)
   }
 
   cadastrar(user: User): Observable<User>{
-    return this.http.post<User>('https://backendthiagofaccipieri.herokuapp.com/usuarios/cadastrar', user)
+    //return this.http.post<User>('https://backendthiagofaccipieri.herokuapp.com/usuarios/cadastrar', user)
 
-    //return this.http.post<User>('https://meublogpessoal.herokuapp.com/usuarios/cadastrar', user)
+    return this.http.post<User>('https://meublogpessoal.herokuapp.com/usuarios/cadastrar', user)
   }
 
   /**
