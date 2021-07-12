a13bd9db9b5e537794bf8423f53986d8d4ac7758
Elton Santos <elton.santos07@outlook.com>
2021-07-12 11:22:38 -0300

    Implementação de pesquisas por tema e título

--- src/app/inicio/inicio.component.ts
+++ src/app/inicio/inicio.component.ts
@@ -18,17 +18,19 @@ export class InicioComponent implements OnInit {
 
   postagem: Postagem = new Postagem()
   listaPostagens: Postagem[]
+  tituloPost: string
 
   tema: Tema = new Tema()
   listaTemas: Tema[]
   idTema: number
+  nomeTema: string
 
   user: User = new User()
   idUser = environment.id
 
   key = 'data'
   reverse = true //Para inverter a ordem de postagem, deixando do mais recente para o mais antigo
-  
+
   constructor(
     private router: Router,
     private postagemService: PostagemService,
@@ -39,9 +41,9 @@ export class InicioComponent implements OnInit {
 
   ngOnInit() {
 
-    window.scroll(0,0)
+    window.scroll(0, 0)
 
-    if(environment.token == ''){
+    if (environment.token == '') {
       //alert('Sua seção expirou, faça o login novamente.')
       this.router.navigate(['/entrar'])
     }
@@ -50,42 +52,68 @@ export class InicioComponent implements OnInit {
     this.getAllPostagens()
   }
 
-  getAllTemas(){
-    this.temaService.getAlltema().subscribe((resp: Tema[])=>{
+  getAllTemas() {
+    this.temaService.getAlltema().subscribe((resp: Tema[]) => {
       this.listaTemas = resp
     })
   }
 
-  findByIdTema(){
-    this.temaService.getByIdTema(this.idTema).subscribe((resp: Tema)=>{
+  findByIdTema() {
+    this.temaService.getByIdTema(this.idTema).subscribe((resp: Tema) => {
       this.tema = resp
     })
   }
 
-  getAllPostagens(){
-    this.postagemService.getAllPostagens().subscribe((resp: Postagem[])=>{
-      this.listaPostagens =resp
+  getAllPostagens() {
+    this.postagemService.getAllPostagens().subscribe((resp: Postagem[]) => {
+      this.listaPostagens = resp
     })
   }
 
-  findByIdUser(){
-    this.authService.getByIdUser(this.idUser).subscribe((resp: User)=>{
+  findByIdUser() {
+    this.authService.getByIdUser(this.idUser).subscribe((resp: User) => {
       this.user = resp
     })
   }
 
-  publicar(){
+  findByTituloPostagem() {
+
+    if (this.tituloPost == '') {
+      this.getAllPostagens()
+    }
+    else {
+      this.postagemService.getByTituloPostagem(this.tituloPost).subscribe((resp: Postagem[]) => {
+        this.listaPostagens = resp
+      })
+    }
+  }
+
+  findByNomeTema(){
+
+    if (this.nomeTema == '') {
+      this.getAllTemas()
+    }
+    else {
+      this.temaService.getByNomeTema(this.tituloPost).subscribe((resp: Tema[]) => {
+        this.listaTemas = resp
+      })
+    }
+  }
+
+  publicar() {
     this.tema.id = this.idTema
     this.postagem.tema = this.tema
     this.user.id = this.idUser
     this.postagem.usuario = this.user
 
-    this.postagemService.postPostagem(this.postagem).subscribe((resp: Postagem)=>{
+    this.postagemService.postPostagem(this.postagem).subscribe((resp: Postagem) => {
       this.postagem = resp
       this.alertas.showAlertSuccess('Postagem realizada com sucesso!')
       this.postagem = new Postagem()
       this.getAllPostagens()
     })
   }
+
+
 
 }
--- src/app/service/tema.service.ts
+++ src/app/service/tema.service.ts
@@ -44,6 +44,10 @@ deleteTema(id: number){
     return this.http.get<Tema>(`https://meublogpessoal.herokuapp.com/tema/${id}`, this.token)
   }
 
+  getByNomeTema(nome: string): Observable<Tema[]>{
+    return this.http.get<Tema[]>(`https://meublogpessoal.herokuapp.com/tema/nome/${nome}`, this.token)
+  }
+
   postTema(tema: Tema): Observable<Tema> {
     return this.http.post<Tema>('https://meublogpessoal.herokuapp.com/tema', tema, this.token)
   }
